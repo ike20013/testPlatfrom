247d78feed1af24ccabb3d7aaefc046cbe00f902
krinjmann <tavklandre@gmail.com>
2020-11-05 03:55:32 +0300

    init

---
+++ server/config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://ike13:10011982@cluster0.pjo8l.mongodb.net/test?retryWrites=true&w=majority',
+    jwtSecret: '5B34C0874DC93FCC390E40FF617B4AB12271DDBA64C7F2A833FDB64BF78F7AA1',
+    stripeSecret: 'sk_test_51HjCrpEbteUUuPkNoq2f8FRfFbLbw1rBnvc3ElOOReL0Ng7vcryLZR2I7I8zOvyaNmfIgp80gzJxf0i21mzyVrfS00TTq9KEVB',
+    endpointSecret: 'whsec_TYbVfxJftVbpWOk0bHl9C7utYfWBqw8I',
+    s3SecretAccessKey: 'Cyk8RH1zxTXmZOcvzDsk9RCGHPTxj+9l9L8QO05W',
+    s3AccessKeyId: 'AKIAIEDTE3Z5KC2JBVUQ',
+    s3PublicBucket: 'teststoregaga',
+    s3PrivateBucket: 'teststoregaga'
+}
